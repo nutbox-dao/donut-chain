39e72c0fcc9d39837c634937ea6394b2da3fae2b
Wenfeng Wang <kalot.wang@gmail.com>
2021-03-12 11:47:06 +0800

    Add event filter

--- app/index.js
+++ app/index.js
@@ -33,8 +33,6 @@ const main = async () => {
         const bridge_sig = '0x' + Buffer.from('dummy signature').toString('hex')
         const bn_decimals = new BN(api.registry.chainDecimals[0]);
 
-        console.log('steem account: ', steem_account)
-
         const issue_donut = async () => {
             console.log('--- Submitting extrinsic to issue DNUT into donut account: ', donut_account.address, ' ---')
 
@@ -79,6 +77,50 @@ const main = async () => {
             })
             nonce += 1
         }
+
+        // Subscribe to system events and look up events we care
+        api.query.system.events((events) => {
+            // console.log(`\nReceived ${events.length} events:`)
+        
+            // Loop through the Vec<EventRecord>
+            events.forEach((record) => {
+                // Extract the phase, event and the event types
+                const { event, phase } = record
+                const types = event.typeDef
+        
+                // Show what we are busy with
+                //   console.log(`\t${event.section}:${event.method}:: (phase=${phase.toString()})`)
+                //   console.log(`\t\t${event.meta.documentation.toString()}`)
+        
+                // DNUT issuing happened
+                if (event.method === 'DonutIssued') {
+                    let arguments = []
+                    event.data.forEach((data, index) => {
+                        arguments.push(data.toString())
+                    })
+
+                    // DonutIssued: Donut Account/Steem Account/Amount
+                    console.log('Details of event DonutIssued ')
+                    console.log(`\t\tDonut Account: ${arguments[0]}`)
+                    console.log(`\t\tSteem Account: ${Buffer.from(arguments[1].slice(2), 'hex').toString()}`)
+                    console.log(`\t\tIssued Amount: ${arguments[2]}`)
+                }
+
+                // DNUT burning happened
+                if (event.method === 'DonutBurned') {
+                    let arguments = []
+                    event.data.forEach((data, index) => {
+                        arguments.push(data.toString())
+                    })
+
+                    // DonutBurned: Donut Account/Steem Account/Amount
+                    console.log('Details of event DonutBurned ')
+                    console.log(`\t\tDonut Account: ${arguments[0]}`)
+                    console.log(`\t\tSteem Account: ${Buffer.from(arguments[1].slice(2), 'hex').toString()}`)
+                    console.log(`\t\tBurned Amount: ${arguments[2]}`)
+                }
+            })
+        })
 
         await issue_donut()
         await burn_donut()
